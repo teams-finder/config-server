1179d1783ac067096a4e517ce6b2a57d1250126d
mrrevo55 <kwolny31@gmail.com>
2022-07-27 22:20:10 +0200

    Add username and password to eureka in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 export VAULT_ADDR='http://127.0.0.1:8200'
 vault secrets disable secret
 vault secrets enable -path=secret kv
-vault write secret/application eureka.instance.hostname=localhost \ eureka.client.service-url.defaultZone=http://localhost:8761/eureka/
+vault write secret/application eureka.instance.hostname=localhost \ eureka.client.service-url.defaultZone=http://eureka:password@localhost:8761/eureka/
